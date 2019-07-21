1d0af71eb429c1a27699848b1b1aa15e6cf4fee2
Hakan Yildiz <hakanyildiz248@gmail.com>
2019-07-21 10:28:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,8 +9,12 @@ function showRepositories(event, data) {
   //we will use JSON.parse here to parse the response string into proper objects !
   const repos = JSON.parse(this.responseText);
   const src = document.getElementById('repository-template').innerHTML;
-  //We're simply building an 'unordered list' by using '.map' to create <li></li> nodes for each object in repos.
+  const template = Handlebars.compile(src);
+  const repoList = template(repos);
+  
+  /*We're simply building an 'unordered list' by using '.map' to create <li></li> nodes for each object in repos.
   const repoList = `<ul>${repos.map ( r => '<li><a href = "' + r.html_url + r.name + '</a></li>' ).join('')}</ul>`; //and we added a link here with '<a></a> tag !'
-  // and now we shoul call the repositories we have created in the previous lines called as 'repoList !'
+  // and now we shoul call the repositories we have created in the previous lines called as 'repoList !'*/
+  
   document.getElementById('repositories').innerHTML = repoList;
 }
