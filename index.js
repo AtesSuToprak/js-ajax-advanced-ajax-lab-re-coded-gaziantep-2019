3fd068fd1173a8c6947796205d0596ce8880f7b8
Hakan Yildiz <hakanyildiz248@gmail.com>
2019-07-21 10:05:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,7 +6,9 @@ function getRepositories(){
 }
 
 function showRepositories(event, data) {
+  //we will use JSON.parse here to parse the response string into proper objects !
   const repos = JSON.parse(this.responseText);
+  //We're simply building an 'unordered list' by using '.map' to create <li></li> nodes for each object in repos.
   const repoList = `<ul>${repos.map ( r => '<li>' + r.name + '</li>' ).join('')}</ul>`;
   document.getElementById('repositories').innerHTML = repoList;
 }
